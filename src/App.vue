5defb08c52fa949a730a95ea5dcc1a896e7ca512
clsung <clsung@wavus.co.kr>
2023-12-27 16:35:58 +0900

    개인 스터디용으로 만든 Spring Boot 기반의 JPA 활용 VUE js 사용

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <MainMap/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainMap from '@/components/MainMap'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainMap
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
 }
 </style>
---
+++ src/components/MainMap.vue
@@ -0,0 +1,49 @@
+<template>
+  <div ref="map" class="map"></div>
+</template>
+
+<script>
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+import { fromLonLat } from 'ol/proj.js';
+import { defaults } from 'ol/control.js';
+
+
+export default {
+  name: 'MainMap',
+  data(){
+    return {
+      olMap: undefined
+    }
+  },
+  mounted() {
+    this.olMap = new Map({
+      target: this.$refs.map,
+      controls : defaults({
+        attribution : false,
+        zoom : false,
+        rotate: false,
+      }),
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: fromLonLat([127.1388684, 37.4449168]),
+        zoom: 12
+      })
+    })
+
+    window.map = this.olMap
+  }
+}
+</script>
+<style scoped>
+.map {
+  width: 100%;
+  height: 100%;
+}
+</style>
